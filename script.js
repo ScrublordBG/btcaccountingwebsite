1c156c8b19e49ea522d22c1d863fa680880b69b0
ScrublordBG <megata10@gmail.com>
2018-06-30 21:12:43 +0300

    coloring and element arrangments

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-var balance;
-$("#money").on('submit',function(e){
-    e.preventDefault();
-    balance = +$('#money-input').val();
-    $('#test').html(balance);
-    console.log(balance);
-});
-$('#deduction').on('submit',function(e){
-    e.preventDefault();
-    balance -= +$('#deduct').val();
-    $('#test').html(balance);
-});
-$(document).ready(function(){
-    console.log("ready");
+$(document).ready(function () {
+    var btcBalance;
+    var btcValue;
+    $.getJSON("https://blockchain.info/de/ticker", function (result) {
+        btcValue = result.USD.last;
+        console.log(btcValue);
+    });
+    $('#chart').hide();
+    $("#initial-balance").on('submit', function (e) {
+        e.preventDefault();
+        btcBalance = +$('#initial-balance-input').val();
+        $('#front-page').slideUp(1000);
+        $('#menu').html("<p>BTC Balance: " + btcBalance + "</p>" + "<p>USD Balance: " + btcBalance * btcValue + "$</p>");
+        $('#chart').fadeIn(2000);
+
+
+        console.log(btcBalance);
+    });
+    $('#deduction').on('submit', function (e) {
+        e.preventDefault();
+        btcBalance -= +$('#deduct').val();
+        $('#menu').html("<p>BTC Balance: " + btcBalance + "</p>" + "<p>USD Balance: " + btcBalance * btcValue + "$</p>");
+    });
+    $('#addition').on('submit', function (e) {
+        e.preventDefault();
+        btcBalance += +$('#add').val();
+        $('#menu').html("<p>BTC Balance: " + btcBalance + "</p>" + "<p>USD Balance: " + btcBalance * btcValue + "$</p>");
+
+    });
+    $(document).ready(function () {
+        console.log("ready");
+    });
 });

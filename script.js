b4eed32560d82eb794d713d160c25565f60f00f8
ScrublordBG <megata10@gmail.com>
2018-07-01 17:06:05 +0300

    added news ticker, alligned divs, added popup text

--- script.js
+++ script.js
@@ -1,33 +1,50 @@
 $(document).ready(function () {
+    $('#myPopup').hide();
     var btcBalance;
     var btcValue;
+    var btcAdd;
+    var tempBalance = btcBalance;
     $.getJSON("https://blockchain.info/de/ticker", function (result) {
         btcValue = result.USD.last;
         console.log(btcValue);
     });
     $('#chart').hide();
+    window.setInterval(function () {
+        if (tempBalance != btcBalance) {
+            $('#menu').html("<p>BTC Balance: " + btcBalance + "</p>" + "<p>USD Balance: " + (btcBalance * btcValue).toFixed(2) + "$</p>");
+            $('#myPopup').html(btcAdd); 
+            $('#myPopup').fadeIn(10, function(){
+                $('#myPopup').fadeOut(500);
+            });
+            tempBalance = btcBalance;
+
+
+        }
+
+    }, 50);
     $("#initial-balance").on('submit', function (e) {
         e.preventDefault();
-        btcBalance = +$('#initial-balance-input').val();
+        btcBalance = btcAdd = +$('#initial-balance-input').val();
         $('#front-page').slideUp(1000);
-        $('#menu').html("<p>BTC Balance: " + btcBalance + "</p>" + "<p>USD Balance: " + btcBalance * btcValue + "$</p>");
         $('#chart').fadeIn(2000);
-
-
         console.log(btcBalance);
     });
     $('#deduction').on('submit', function (e) {
         e.preventDefault();
-        btcBalance -= +$('#deduct').val();
-        $('#menu').html("<p>BTC Balance: " + btcBalance + "</p>" + "<p>USD Balance: " + btcBalance * btcValue + "$</p>");
+        btcBalance -= btcAdd = +$('#deduct').val();
     });
     $('#addition').on('submit', function (e) {
         e.preventDefault();
-        btcBalance += +$('#add').val();
-        $('#menu').html("<p>BTC Balance: " + btcBalance + "</p>" + "<p>USD Balance: " + btcBalance * btcValue + "$</p>");
+        btcBalance += btcAdd = +$('#add').val();
+
+    });
+    $('#add-one, #add-zerotwentyfive, #remove-one, #remove-zerotwentyfive').click(function () {
+        btcBalance += btcAdd = +$(this).val();
+
 
     });
     $(document).ready(function () {
         console.log("ready");
     });
+
 });

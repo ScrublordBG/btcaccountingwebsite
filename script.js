586b8deec8fe7f5209ba78b603b521bb5518c24b
ScrublordBG <megata10@gmail.com>
2018-07-02 13:56:26 +0300

    real time price updates

--- script.js
+++ script.js
@@ -3,21 +3,31 @@ $(document).ready(function () {
     var btcBalance;
     var btcValue;
     var btcAdd;
+    var tempValue = btcValue;
     var tempBalance = btcBalance;
-    $.getJSON("https://blockchain.info/de/ticker", function (result) {
-        btcValue = result.USD.last;
-        console.log(btcValue);
-    });
+    /* $.getJSON("https://blockchain.info/de/ticker", function (result) {
+         btcValue = result.USD.last;
+         console.log(btcValue);
+     });*/
+
     $('#chart').hide();
     window.setInterval(function () {
+        btcValue = +$('.btcwdgt-body').find('.price').find('.p-wrap').find('p').html();
         if (tempBalance != btcBalance) {
-            $('#menu').html("<p>BTC Balance: " + btcBalance.toFixed(5) + "</p>" + "<p>USD Balance: " + (btcBalance * btcValue).toFixed(2) + "$</p>")
-            $('#myPopup').html(btcAdd).css('color',btcAdd>0?'green':'red'); 
-            $('#myPopup').fadeIn(10, function(){
+            $('#menu').html("<p>BTC Balance: " + btcBalance.toFixed(5) + "</p>" + "<p>USD Balance: " + (btcBalance * btcValue).toFixed(2) + "$</p>");
+            $('#myPopup').html(btcAdd).css('color', btcAdd > 0 ? 'green' : 'red').css('bottom','87%');
+            $('#myPopup').fadeIn(10, function () {
                 $('#myPopup').fadeOut(500);
             });
             tempBalance = btcBalance;
-
+        }
+        if (tempValue != btcValue){
+            $('#menu').html("<p>BTC Balance: " + btcBalance.toFixed(5) + "</p>" + "<p>USD Balance: " + (btcBalance * btcValue).toFixed(2) + "$</p>");
+            $('#myPopup').html(btcValue*btcBalance).css('color', btcValue > tempValue ? 'green' : 'red').css('bottom','73%');
+            $('#myPopup').fadeIn(10, function () {
+                $('#myPopup').fadeOut(1500);
+            });
+            tempValue = btcValue;
 
         }
 
